537cf228ac74e74dd16e4f78ee516ae5508ab1f3
zhizuio <zhi.zhao@medisin.uio.no>
2024-11-14 15:31:17 +0100

    fix testthat issue

--- tests/testthat/test-general.R
+++ tests/testthat/test-general.R
@@ -13,7 +13,7 @@ fit <- GPTCM(dat, n, p, L, nIter = 50, burnin = 10)
 test_that("fit has properly class and length", {
   expect_s3_class(fit, "GPTCM")
   expect_length(fit, 3L)
-  expect_length(fit$input, 3L)
+  expect_length(fit$input, 4L)
   expect_length(fit$input$hyperpar, 8L)
   expect_length(fit$output, 2L)
   expect_length(fit$output$posterior, 5L)
